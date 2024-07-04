1032c3d69822a24d207611b69b07acf05ca9638f
KyryloMuzychka <kyryll2016@gmail.com>
2024-07-05 00:14:41 +0300

    feat: added 3 hourly page

--- script/script.js
+++ script/script.js
@@ -10,7 +10,7 @@ async function getCityByCoordinates() {
   try {
     const geoPosition = new GeoPosition(
       "https://maps.googleapis.com/maps/api/geocode/json",
-      "***"
+      "*"
     );
     return await geoPosition.getCity();
   } catch (error) {
@@ -26,7 +26,7 @@ async function setDate() {
 async function setCurrentWeather() {
   let forecast = new Forecast(
     "https://api.openweathermap.org/data/2.5/weather",
-    "***"
+    "*"
   );
   let data = await forecast.getCurrentWeather(page.getCity());
   if (data) {
@@ -36,14 +36,71 @@ async function setCurrentWeather() {
   }
 }
 
+function showWeatherForDay(data, dateCurrent) {
+  $("#date").empty();
+  $("#forecast").empty();
+  $("#temp").empty();
+  $("#realFeel").empty();
+  $("#wind").empty();
+
+  $("#date").append($("<td></td>").text("day"));
+  $("#forecast").append($("<td></td>").text("Forecast"));
+  $("#temp").append($("<td></td>").html("Temp (&#8451;)"));
+  $("#realFeel").append($("<td></td>").text("RealFeel"));
+  $("#wind").append($("<td></td>").text("Wind (km/h)"));  
+
+  data.list.forEach((element) => {
+
+    let weather_main = $("<td></td>");
+    let weather_icon = $("<img>", {
+      src:
+        "https://openweathermap.org/img/wn/" +
+        element.weather[0].icon +
+        ".png",
+    });
+    let waether_title = $("<span></span>").text(element.weather[0].main);
+    let date = $("<td></td>").text(element.dt_txt);
+    let temp = $("<td></td>").text(element.main["temp"]);
+    let feels_like = $("<td></td>").text(element.main["feels_like"]);
+    let wind = $("<td></td>").text(element.wind["speed"]);
+    
+    if (element.dt_txt.split(" ")[0] == dateCurrent) {
+      weather_main.append(weather_icon);
+      weather_main.append(waether_title);
+      $("#date").append(date);
+      $("#forecast").append(weather_main);
+      $("#temp").append(temp);
+      $("#realFeel").append(feels_like);
+      $("#wind").append(wind);
+    }
+  });
+} 
+
 async function getFiveDayWeather() {
-  let forecast = new Forecast(    
+  let forecast = new Forecast(
     "https://api.openweathermap.org/data/2.5/forecast",
-    "***"
+    "*"
   );
   let data = await forecast.getFiveDayWeather(page.getCity());
   if (data) {
     console.log(data);
+
+    let dateArray = [];
+    data.list.forEach((element) => {
+      let date = element.dt_txt.split(" ")[0];
+      if (!dateArray.includes(date)) {
+        dateArray.push(date);
+      }
+    });
+
+    dateArray.forEach((element) => {
+      let block = $("<div></div>").text(element).click((event) => {showWeatherForDay(data, element)});
+      $("#days").append(block);
+    });
+
+    console.log(dateArray);
+
+   
   } else {
     page.showError();
   }
